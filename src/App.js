b7499c50e5362a233e4d273a42027de80288a57d
Yadav <yadadip@apac.corpdir.net>
2023-06-23 23:29:14 +0530

    modified everything

--- src/App.js
+++ src/App.js
@@ -1,44 +1,24 @@
 import './App.css';
-import axios from "axios";
-import { useState, useEffect } from "react";
+import Form from "./components/Form.jsx";
+import UserTable from "./components/UserTable.jsx";
 
 function App() {
 
-  const [name, setName] = useState("");
-  const [phone, setPhone] = useState("");
-  const [email, setEmail] = useState("");
-  const [response, setResponse] = useState("");
-
-
-  function handleSubmit(e) {
-    e.preventDefault();
-    const obj = {
-      name,
-      phone,
-      email
-    }
-    console.log(obj);
-
-    axios.get("http://localhost:5000/getUser")
-    .then(res => {
-      console.log("inside then", res);
-      setResponse(res.data.message.email);
-      setName("");
-      setPhone("");
-      setEmail("");
-    })
-    .catch(err => console.log(err));
-  }
   return (
     <div className="container">
-      <h1> User's Directory </h1>
-      <input type="text" value={name} placeholder="name" onChange={e => setName(e.target.value)} />
-      <input type="number" value={phone} placeholder="phone" onChange={e => setPhone(e.target.value)} />
-      <input type="email" value={email} placeholder="email" onChange={e => setEmail(e.target.value)} />
-      <button onClick={handleSubmit}> Submit </button>
-      { response? <h2> {response} </h2> : null}
+
+      <h1> User's Directory 1</h1>
+
+      <Form />
+    
+      <UserTable/>
+
     </div>
   );
 }
 
 export default App;
+
+
+
+
